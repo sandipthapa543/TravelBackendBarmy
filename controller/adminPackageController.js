a183298fe0c3011900f208156667fddd8f613962
Om Krishna Thapa <happyom4@gmail.com>
2020-06-28 14:30:18 +0545

    adminPackageController.js created

---
+++ controller/adminPackageController.js
@@ -0,0 +1,5 @@
+const db = require("../model/index");
+//Package class
+class Packages {}
+
+module.exports = Packages;
--- model/index.js
+++ model/index.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(`postgres://${process.env.UNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DB}`,{
+const sequelize = new Sequelize(
+  `postgres://${process.env.UNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DB}`,
+  {
+    dialect: "postgres",
 
-    dialect:'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 3000,
+      idle: 10000,
+    },
+  }
+);
 
-    pool:{
-        max:5,
-        min:0,
-        acquire:3000,
-        idle:10000
-    }
-});
+const db = {};
 
-const db={}
-
- db.Sequelize=Sequelize;
- db.sequelize=sequelize;
- db.users=  require("./user.model.js")(sequelize,Sequelize);
-
- module.exports = db;
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.users = require("./user.model.js")(sequelize, Sequelize);
+db.packages = require("./admin.package.js")(sequelize, Sequelize);
+module.exports = db;
