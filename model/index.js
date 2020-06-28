9f60af620072b6ea77751390babf65d596ef5c39
Om Krishna Thapa <happyom4@gmail.com>
2020-06-28 12:41:44 +0545

    Signup and Register testing successful.

--- model/index.js
+++ model/index.js
@@ -1,22 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(`postgres://${process.env.UNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DB}`,{
+const sequelize = new Sequelize(
+  `postgres://${process.env.UNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DB}`,
+  {
+    logging: false,
+    dialect: "postgres",
 
-    dialect:'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 3000,
+      idle: 10000,
+    },
+  }
+);
 
-    pool:{
-        max:5,
-        min:0,
-        acquire:3000,
-        idle:10000
-    }
-});
+const db = {};
 
-const db={}
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.users = require("./user.model.js")(sequelize, Sequelize);
 
- db.Sequelize=Sequelize;
- db.sequelize=sequelize;
- db.users=  require("./user.model.js")(sequelize,Sequelize);
-
- module.exports = db;
+module.exports = db;
