78f23d06f84bca3e7165c23ecd5773f65f1f641b
Samyam Dabadi <samyamdw@gmail.com>
2020-06-28 09:58:14 +0545

    Login testing

--- UnitTest/userTest.js
+++ UnitTest/userTest.js
@@ -1,41 +1,61 @@
-const {expect} = require('chai');
-// const app = require('supertest')(require('../app'))
+const { expect } = require("chai");
+const request = require('supertest')
+const app = (require("../app"));
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(`postgres://postgres:postgres@localhost/TravelTest`,{
-    dialect:'postgres',
+const sequelize = new Sequelize(
+  `postgres://postgres:postgres@localhost/TravelTest`,
+  {
+    dialect: "postgres",
 
-    pool:{
-        max:5,
-        min:0,
-        acquire:3000,
-        idle:10000
-    }
-});
-const db={}
-db.Sequelize=Sequelize;
- db.sequelize=sequelize;
-db.users=  require("../model/user.model.js")(sequelize,Sequelize);
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 3000,
+      idle: 10000,
+    },
+  }
+);
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.users = require("../model/user.model.js")(sequelize, Sequelize);
 // db.sequelize.sync({ force: true })
 db.sequelize.sync();
 
-describe('Register TDD', () => {
-// User registration test
-it('The following user will be added', () => {
+describe("Register TDD", () => {
+  // User registration test
+  it("The following user will be added", () => {
     // .
-    return db.users.create({
-        First_Name: 'Om Krishna',
-        Last_Name:'Thapa',
-        Email:'othapa@gmail.com',
-        Password:'password',
-        Country:'Nepal',
-        City: 'Kathmandu',
-        Contact_No: '9876543210'
-    }).then(result => {
-            expect(result.First_Name).to.equal('Om Krishna');
-            expect(result.Last_Name).to.equal('Thapa');
-            expect(result.Email).to.equal('othapa@gmail.com');
-            expect(result.Country).to.equal('Nepal');
-        });
-})
-})
+    return db.users
+      .create({
+        First_Name: "Om Krishna",
+        Last_Name: "Thapa",
+        Email: "othapa@gmail.com",
+        Password: "password",
+        Country: "Nepal",
+        City: "Kathmandu",
+        Contact_No: "9876543210",
+      })
+      .then((result) => {
+        expect(result.First_Name).to.equal("Om Krishna");
+        expect(result.Last_Name).to.equal("Thapa");
+        expect(result.Email).to.equal("othapa@gmail.com");
+        expect(result.Country).to.equal("Nepal");
+      });
+  });
+});
+
+describe("Login TDD", () => {
+  // User registration test
+  it("It should login and generate jwt", () => {
+    // .
+    request(app)
+      .post("/user/login")
+      .send({ email: "othapa@gmail.com", password: "password" })
+      .expect(200)
+      .expect(res => {
+          expect(res.headers[x-auth]).not.toBeNull();
+      })
+  });
+});
