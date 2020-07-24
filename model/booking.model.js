9af98a91b6afa335a757b61eabf80aec0612643f
Om Krishna Thapa <happyom4@gmail.com>
2020-07-24 17:57:51 +0545

    booking model added

---
+++ model/booking.model.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize) => {
+  const Booking = sequelize.define("bookings", {
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    package_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    People: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    Status: {
+      type: Sequelize.ENUM,
+      defaultValue: "pending",
+      values: ["pending", "booked ", "cancel "],
+    },
+    departure_dates: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  });
+  return Booking;
+};
