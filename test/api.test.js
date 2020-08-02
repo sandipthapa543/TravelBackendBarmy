8540038589d50237b30e412f5c4f8216663de066
Om Krishna Thapa <happyom4@gmail.com>
2020-08-02 12:49:42 +0545

    More test added

--- test/api.test.js
+++ test/api.test.js
@@ -99,6 +99,174 @@ describe("-/User", () => {
         .set("Authorization", "Bearer " + token)
         .end((err, res) => {
           res.should.have.status(200);
+          // res.body.should.be.a("Array");
+          res.body.should.be.a("object");
+
+          done();
+        });
+    });
+  });
+
+  // POST path not defined properly
+  describe("/POST user inquiry package", () => {
+    it("Should show path not defined properly", (done) => {
+      const data = {
+        user_id: 5,
+        package_id: 3,
+        People: 1,
+        Message: "Weee",
+      };
+      chai
+        .request(app)
+        .post("/user/inquirys/")
+        .set("Authorization", "Bearer ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // POST without bearer
+  describe("/POST user inquiry package", () => {
+    it("Should show an error without bearer", (done) => {
+      const data = {
+        user_id: 5,
+        package_id: 3,
+        People: 1,
+        Message: "Weee",
+      };
+      chai
+        .request(app)
+        .post("/user/inquiry/")
+        .set("Authorization", " ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // POST without token
+  describe("/POST user inquiry package", () => {
+    it("Should show an error for not having id", (done) => {
+      const data = {
+        user_id: 5,
+        package_id: 3,
+        People: 1,
+        Message: "Weee",
+      };
+      chai
+        .request(app)
+        .post("/user/inquiry/")
+        .set("Authorization", "Bearer ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // POST inquiry pass
+  describe("/POST user", () => {
+    it("Should add user inquiry package", (done) => {
+      const data = {
+        user_id: 5,
+        package_id: 3,
+        People: 1,
+        Message: "Weee",
+      };
+      chai
+        .request(app)
+        .post("/user/inquiry")
+        .set("Authorization", "Bearer " + token)
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          // expect(res.body.message).to.contain("Inquiry added");
+          done();
+        });
+    });
+  });
+
+  // PUT user inquiry
+  describe("/PUT user inquiry", () => {
+    it("Should show an error in inquiry info", (done) => {
+      const inquiry = {
+        status: "pending",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .put("/user/inquiry/", +id)
+        .send(inquiry)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT inquiry
+  describe("/PUT user inquiry", () => {
+    it("Should show id not found", (done) => {
+      const inquiry = {
+        status: "pending",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .put("/user/inquiry/")
+        .send(inquiry)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT inquiry
+  describe("/PUT user inquiry", () => {
+    it("Should show path not defined", (done) => {
+      const inquiry = {
+        status: "pending",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .put("/user/inquirys/", +id)
+        .send(inquiry)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT inquiry pass
+  describe("/PUT user inquiry", () => {
+    it("Should update user inquiry", (done) => {
+      const inquiry = {
+        status: "pending",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .patch("/user/inquiry/:id", +id)
+        .set("Authorization", "Bearer " + token)
+        .send(inquiry)
+        .end((err, res) => {
+          res.should.have.status(200);
           res.body.should.be.a("Array");
           done();
         });
@@ -163,8 +331,8 @@ describe("-/Package", () => {
     });
   });
 
-  //delete routes testing
-  describe("/Delete/:id packages", () => {
+  // Delete routes testing
+  describe("/Delete package", () => {
     it("Should not delete the packages info", (done) => {
       const id = 6;
       chai
@@ -195,6 +363,68 @@ describe("-/Blog", () => {
     });
   });
 
+  // POST blog
+
+  // POST path not defined properly
+  describe("/POST Blog Comment", () => {
+    it("Should show path not defined properly", (done) => {
+      const data = {
+        blog_id: 4,
+        Comments: "TDD comment test.",
+      };
+      chai
+        .request(app)
+        .post("/blog/comments")
+        .set("Authorization", "Bearer ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // POST without bearer
+  describe("/POST Blog Comment", () => {
+    it("Should show an error without bearer", (done) => {
+      const data = {
+        blog_id: 4,
+        Comments: "TDD comment test.",
+      };
+      chai
+        .request(app)
+        .post("/blog/comment")
+        .set("Authorization", " ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // POST without token
+  describe("/POST Blog Comment", () => {
+    it("Should show an error for not having id", (done) => {
+      const data = {
+        blog_id: 4,
+        Comments: "TDD comment test.",
+      };
+      chai
+        .request(app)
+        .post("/blog/comment")
+        .set("Authorization", "Bearer ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
   //Api test, POST blog comment
   describe("/POST Blog Comment", () => {
     it("Should POST blog", (done) => {
@@ -216,25 +446,100 @@ describe("-/Blog", () => {
     });
   });
 
+  // PUT
+  // PUT blog
+  describe("/PUT blog", () => {
+    it("Should show an error in blog info", (done) => {
+      const blog = {
+        Comments: "m",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .put("/user/inquiry/", +id)
+        .send(blog)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT blog
+  describe("/PUT user blog", () => {
+    it("Should show id not found", (done) => {
+      const blog = {
+        Comments: "m",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .put("/blog/edit/")
+        .send(blog)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT blog
+  describe("/PUT user blog", () => {
+    it("Should show path not defined", (done) => {
+      const blog = {
+        Comments: "m",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .put("/blog/edit/", +id)
+        .send(blog)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT blog pass
   describe("/PUT/:id blog", () => {
-    it("Should update the packages info", (done) => {
-      const packages = {
+    it("Should update the blog info", (done) => {
+      const blog = {
         Comments: "m",
       };
       const id = 5;
       chai
         .request(app)
         .put("/blog/edit/:id" + id)
-        .send(packages)
+        .send(blog)
         .end((err, res) => {
           res.should.have.status(200);
-          //  res.body.should.be.a("object");
+          res.body.should.be.a("Array");
           done();
         });
     });
   });
 
-  describe("/Delete/:id blog", () => {
+  //DELETE
+  describe("/Delete blog", () => {
+    it("Should show path not found", (done) => {
+      const id = 2;
+      chai
+        .request(app)
+        .delete("/blogs/:id" + id)
+
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+  // Delete blog
+  describe("/Delete blog", () => {
     it("Should delete the blog info", (done) => {
       const id = 2;
       chai
@@ -265,7 +570,73 @@ describe("-/Booking", () => {
     });
   });
 
-  // POST the bookings
+  // POST path not defined properly
+  describe("/POST Bookings", () => {
+    it("Should show path not defined properly", (done) => {
+      const data = {
+        user_id: 5,
+        package_id: 3,
+        People: 1,
+        departure_dates: "02/02/2020",
+      };
+      chai
+        .request(app)
+        .post("/bookings/")
+        .set("Authorization", "Bearer ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // POST withoud bearer
+  describe("/POST Bookings", () => {
+    it("Should show an error without bearer", (done) => {
+      const data = {
+        user_id: 5,
+        package_id: 3,
+        People: 1,
+        departure_dates: "02/02/2020",
+      };
+      chai
+        .request(app)
+        .post("/booking/")
+        .set("Authorization", " ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // POST without token
+  describe("/POST Bookings", () => {
+    it("Should show an error for not having id", (done) => {
+      const data = {
+        user_id: 5,
+        package_id: 3,
+        People: 1,
+        departure_dates: "02/02/2020",
+      };
+      chai
+        .request(app)
+        .post("/booking/")
+        .set("Authorization", "Bearer ")
+        .send(data)
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // POST the bookings pass
   describe("/POST Bookings", () => {
     it("Should add booking", (done) => {
       const data = {
@@ -289,18 +660,75 @@ describe("-/Booking", () => {
 
   // PUT bookings
   describe("/PUT Booking", () => {
-    it("Should update booking info", (done) => {
-      const packages = {
+    it("Should show an error in booking info", (done) => {
+      const bookings = {
         status: "pending",
       };
       const id = 5;
       chai
         .request(app)
         .put("/booking/", +id)
-        .send(packages)
+        .send(bookings)
         .end((err, res) => {
           res.should.have.status(404);
           res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT bookings
+  describe("/PUT Booking", () => {
+    it("Should show id not found", (done) => {
+      const bookings = {
+        status: "pending",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .put("/booking/")
+        .send(bookings)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT bookings
+  describe("/PUT Booking", () => {
+    it("Should show path not defined", (done) => {
+      const bookings = {
+        status: "pending",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .put("/bookings/", +id)
+        .send(bookings)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          done();
+        });
+    });
+  });
+
+  // PUT bookings pass
+  describe("/PUT Booking", () => {
+    it("Should update booking info", (done) => {
+      const bookings = {
+        status: "pending",
+      };
+      const id = 5;
+      chai
+        .request(app)
+        .patch("/booking/:id", +id)
+        .send(bookings)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("Array");
           done();
         });
     });
